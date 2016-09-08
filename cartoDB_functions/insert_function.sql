243c76911acd40ed9e4d075717db3c894adb0f12
zrobby <zach.m.robinson@gmail.com>
2016-09-08 13:13:21 -0700

    Add files via upload

--- cartoDB_functions/insert_function.sql
+++ cartoDB_functions/insert_function.sql
@@ -1,18 +1,18 @@
 DROP FUNCTION IF EXISTS insert_bikeways_data(text,text,text,text);
---Assumes only one value being inserted
+-- Assumes only one value being inserted
 
 CREATE OR REPLACE FUNCTION insert_bikeways_data (
     _geojson TEXT,
     _notes TEXT,
     _name TEXT,
     _zip TEXT)
---Has to return something in order to be used in a "SELECT" statement
+-- Has to return something in order to be used in a "SELECT" statement
 RETURNS integer
 AS $$
 DECLARE 
     _the_geom GEOMETRY;
 BEGIN
-    --Convert the GeoJSON to a geometry type for insertion. 
+    -- Convert the GeoJSON to a geometry type for insertion. 
     _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
 
     EXECUTE ' INSERT INTO crowdmap_basic (the_geom, notes, name, zipcode)
@@ -24,5 +24,5 @@ END;
 $$
 LANGUAGE plpgsql SECURITY DEFINER ;
 
---Grant access to the public user
+-- Grant access to the public user
 GRANT EXECUTE ON FUNCTION insert_bikeways_data(text,text,text,text) TO publicuser;
